470989120b34f9cc17caa940cd75052cbc0ad2cb
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2022-05-24 19:17:46 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread (Mulitply);         
+            thread.Start();                              
+            thread.Join();
+            for (int i = 1; i < 16; i++)
+            {
+                Console.WriteLine(i + " X 15 = " + (i * 15));
+            }
+
+        }
+ 
+        static void Mulitply()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i + " X 10 = " + (i * 10));
+            }
+        }
+    }
+}
